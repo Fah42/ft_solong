d9675f6f6da560f22ca1913b17cd2b9444ebe99c
Farid HADHRI <fhadhri@e1r12p10.clusters.42paris.fr>
2022-08-26 12:29:24 +0200

    t

--- so_long.h
+++ so_long.h
@@ -1,1 +1,19 @@
-#include <mlx.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fhadhri <fhadhri@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/26 12:24:54 by fhadhri           #+#    #+#             */
+/*   Updated: 2022/08/26 12:26:24 by fhadhri          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# include <mlx.h>
+# include "libft/libft.h"
+# include "ft_printf/ft_printf.h"
+#endif
